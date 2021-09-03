963c0029417f868e9c32c1ab2c01fafcb277ec7c
Risa Tone <mmm5ar07dd@gmail.com>
2021-09-03 23:24:46 +0900

    update #8 SQLインジェクションへの対策

--- www/html/login.php
+++ www/html/login.php
@@ -1,11 +1,17 @@
 <?php
+// 定数ファイルの読み込み
 require_once '../conf/const.php';
+// 汎用関数ファイルの読み込み
 require_once MODEL_PATH . 'functions.php';
 
+// ログインチェックを行うためセッションを開始する
 session_start();
 
+// ログインチェック用の関数の利用
 if(is_logined() === true){
+  // ログインしている場合商品一覧ページへリダイレクト
   redirect_to(HOME_URL);
 }
 
+// ビューの読み込み
 include_once VIEW_PATH . 'login_view.php';
--- www/html/signup.php
+++ www/html/signup.php
@@ -1,13 +1,18 @@
 <?php
+// 定数ファイルの読み込み
 require_once '../conf/const.php';
+// 汎用関数ファイルの読み込み
 require_once MODEL_PATH . 'functions.php';
 
+// ログインチェックを行うためセッションを開始する
 session_start();
-
+// ログインチェック用の関数を利用
 if(is_logined() === true){
+  // ログインしている場合は商品一覧ページにリダイレクト
   redirect_to(HOME_URL);
 }
 
+// ビューの読み込み
 include_once VIEW_PATH . 'signup_view.php';
 
 
